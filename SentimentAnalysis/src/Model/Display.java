852e8c95ecb3f3d55b6f7fefab97d352e93f8d89
sammed mangave <105945906+samycolen@users.noreply.github.com>
2024-06-08 08:45:27 +0530

    Delete SentimentAnalysis directory

--- SentimentAnalysis/src/Model/Display.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-public class Display {
-    public static void showSentimentResult(String sentimentLabel) {
-        // Display the result
-        System.out.println("Sentiment: " + sentimentLabel);
-    }
-}
